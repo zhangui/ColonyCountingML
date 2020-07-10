fa76fa10d1d064d3755aff52026af46142ddd4f3
jpark837 <67569007+jpark837@users.noreply.github.com>
2020-07-10 01:48:38 -0400

    need to add threshold adjuster

--- Binary.py
+++ Binary.py
@@ -1,6 +1,24 @@
 import numpy as np
-from PIL import Image
+import os
+import cv2
 
-img = Image.open('testrgba.png')
-array = np.array(img)
-print(array.shape)      # (100, 200, 4)
+def get_path(i):
+
+    script_dir = os.path.dirname(__file__)
+    rel_path = "".join(["colony images/colony image (",str(i),").jpg"])
+    abs_file_path = os.path.join(script_dir, rel_path)#.replace("/","\\")
+    os.path.normpath(abs_file_path)
+
+    return abs_file_path
+
+def conv_to_binary():
+
+    abs_file_path = get_path(8)
+    img = cv2.imread(abs_file_path,2)
+    print(img.size)
+    ret, bw_img = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+    cv2.imshow('Binary Image',bw_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+conv_to_binary()
