1379b5b09807cd59b5864ce15f53c599d2418c47
jpark837 <67569007+jpark837@users.noreply.github.com>
2020-07-14 21:33:48 -0400

    works but need to interpret xtrain and ytrain

--- gradient_boosting.py
+++ gradient_boosting.py
@@ -1,16 +1,30 @@
-import sklearn
-print(sklearn.__version__)
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, ensemble
+from sklearn.inspection import permutation_importance
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import os
+import numpy as np
 
-#X_train, X_test, y_train, y_test = load data here
+script_dir = os.path.dirname(__file__)
+abs_file_path = os.path.join(script_dir, "Colony Count.xlsx")
+os.path.normpath(abs_file_path)
 
+df = pd.read_excel(abs_file_path, sheet_name='Sheet1')
+# print(df.values)
+X_train, X_test, y_train, y_test = np.ones((32,2))*0.5, np.ones((32,2)), df.values[:,2]*0.5 + df.values[:,3]*0.5, df.values[:,1]
+
+print(y_train)
 params = {'n_estimators': 500,
           'max_depth': 4,
           'min_samples_split': 5,
           'learning_rate': 0.01,
           'loss': 'ls'}
 
-gradient_boosting_regressor_model = ensemble.GradientBoostingRegressor(**params)
-gradient_boosting_regressor_model.fit(X_train, y_train)
+reg = ensemble.GradientBoostingRegressor(**params)
+reg.fit(X_train, y_train)
 
 mse = mean_squared_error(y_test, reg.predict(X_test))
 print("The mean squared error (MSE) on test set is: {:.4f}".format(mse))
@@ -18,6 +32,8 @@ print("The mean squared error (MSE) on test set is: {:.4f}".format(mse))
 test_score = np.zeros((params['n_estimators'],), dtype=np.float64)
 for i, y_pred in enumerate(reg.staged_predict(X_test)):
     test_score[i] = reg.loss_(y_test, y_pred)
+
+# for checking number of n_estimators
 
 fig = plt.figure(figsize=(6, 6))
 plt.subplot(1, 1, 1)
@@ -39,3 +55,6 @@ fig = plt.figure(figsize=(12, 6))
 plt.barh(pos, feature_importance[sorted_idx], align='center')
 plt.yticks(pos, ['kmeans', 'CNN'])
 plt.title('Feature Importance (MDI)')
+plt.show()
+
+yhat = model.predict([0.5, 0.5])
