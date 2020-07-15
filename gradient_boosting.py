3e5c98e938006e618d7109d475966c32da506b1c
jpark837 <67569007+jpark837@users.noreply.github.com>
2020-07-14 22:32:30 -0400

    fixed, working

--- gradient_boosting.py
+++ gradient_boosting.py
@@ -6,7 +6,6 @@ from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import os
-import numpy as np
 
 script_dir = os.path.dirname(__file__)
 abs_file_path = os.path.join(script_dir, "Colony Count.xlsx")
@@ -14,9 +13,10 @@ os.path.normpath(abs_file_path)
 
 df = pd.read_excel(abs_file_path, sheet_name='Sheet1')
 # print(df.values)
-X_train, X_test, y_train, y_test = np.ones((32,2))*0.5, np.ones((32,2)), df.values[:,2]*0.5 + df.values[:,3]*0.5, df.values[:,1]
+X = df.values[:,range(2,4)]
+y = df.values[:,1]
+X_train, X_test, y_train, y_test = X,X,y,y
 
-print(y_train)
 params = {'n_estimators': 500,
           'max_depth': 4,
           'min_samples_split': 5,
@@ -26,29 +26,18 @@ params = {'n_estimators': 500,
 reg = ensemble.GradientBoostingRegressor(**params)
 reg.fit(X_train, y_train)
 
-mse = mean_squared_error(y_test, reg.predict(X_test))
-print("The mean squared error (MSE) on test set is: {:.4f}".format(mse))
 
 test_score = np.zeros((params['n_estimators'],), dtype=np.float64)
 for i, y_pred in enumerate(reg.staged_predict(X_test)):
     test_score[i] = reg.loss_(y_test, y_pred)
 
-# for checking number of n_estimators
-
-fig = plt.figure(figsize=(6, 6))
-plt.subplot(1, 1, 1)
-plt.title('Deviance')
-plt.plot(np.arange(params['n_estimators']) + 1, reg.train_score_, 'b-',
-         label='Training Set Deviance')
-plt.plot(np.arange(params['n_estimators']) + 1, test_score, 'r-',
-         label='Test Set Deviance')
-plt.legend(loc='upper right')
-plt.xlabel('Boosting Iterations')
-plt.ylabel('Deviance')
-fig.tight_layout()
-plt.show()
-
 feature_importance = reg.feature_importances_
+print("the feature importance is", feature_importance)
+pred = X[:,0]*feature_importance[0] + X[:,1]*feature_importance[1]
+mse = mean_squared_error(y, pred)
+print("The boosting prediction is", pred)
+print("The mean squared error (MSE) on test set is: {:.4f}".format(mse))
+
 sorted_idx = np.argsort(feature_importance)
 pos = np.arange(sorted_idx.shape[0]) + .5
 fig = plt.figure(figsize=(12, 6))
@@ -57,5 +46,8 @@ plt.yticks(pos, ['kmeans', 'CNN'])
 plt.title('Feature Importance (MDI)')
 plt.show()
 
-yhat = reg.predict([[0.5, 0.5],[0.5,0.5]])
-print(yhat)
+#Write result to array
+df = pd.DataFrame(pred)
+abs_file_path = os.path.join(script_dir, "BoostingResult.xlsx")
+os.path.normpath(abs_file_path)
+df.to_excel(excel_writer = abs_file_path)
